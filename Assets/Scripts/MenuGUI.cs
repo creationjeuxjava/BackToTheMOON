6e09b51f42b5eab3d8c19de99d508bb09dc66bb8
alto971 <gwadaldesign@gmail.com>
2014-08-11 17:22:38 +0200

    position GUI

--- Assets/Scripts/MenuGUI.cs
+++ Assets/Scripts/MenuGUI.cs
@@ -15,7 +15,7 @@ public class MenuGUI : MonoBehaviour {
 
 	void OnGUI () {
 		// Make a group on the center of the screen
-		GUI.BeginGroup (new Rect (Screen.width / 2 - 50, Screen.height / 2 - 50, 100, 500));
+		GUI.BeginGroup (new Rect (Screen.width / 2 - 50, Screen.height / 2 - 50, 500, 1000));
 		// All rectangles are now adjusted to the group. (0,0) is the topleft corner of the group.
 		
 		// We'll make a box so you can see where the group is on-screen.
