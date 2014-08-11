635fa2018eb02438e91653890b56c964d72a2335
alto971 <gwadaldesign@gmail.com>
2014-08-12 00:13:04 +0200

    Travail sur PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,12 @@ public class PlayerController : MonoBehaviour {
     bool isMoving;
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log("PlayerController-->création");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Vector3 touchPosition = Input.GetTouch(0).position;
+		Debug.Log("PlayerController: on touche écran en  ("+touchPosition.x+" , "+touchPosition.y+")");
 	}
 }
