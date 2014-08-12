7ec52b828399565025d055ebc84f7215485cd898
alto971 <gwadaldesign@gmail.com>
2014-08-12 13:21:55 +0200

    Résolution dus coordonnées screen et world
    détection du clic sur le personnage

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
     bool isMoving;
+	public Camera camera;
+
 	// Use this for initialization
 	void Start () {
 		Debug.Log("PlayerController-->création");
@@ -14,22 +16,29 @@ public class PlayerController : MonoBehaviour {
 		/*** android ****/
 		if (Input.touchCount == 1) {
 			Vector3 touchPosition = Input.GetTouch (0).position;
+			touchPosition = camera.ScreenToWorldPoint(touchPosition);
 			Debug.Log ("PlayerController: on touche écran en  (" + touchPosition.x + " , " + touchPosition.y + ")");
+
+			//affichage pour debug
+			InGameGUI.setMessage("perso centre en ("+gameObject.collider2D.bounds.center.x+" , "+gameObject.collider2D.bounds.center.y+")",
+			                     "PlayerController: on touche écran en  (" + touchPosition.x + " , " + touchPosition.y + ")");
 		}
 		/**** version pc *****/
-		if (Input.GetMouseButton (0)) {
+		if (Input.GetMouseButton (0)&& Input.mousePresent) {
 			Vector3 touchPosition = Input.mousePosition;
+			//touchPosition = camera.WorldToScreenPoint(target.position);
+			touchPosition = camera.ScreenToWorldPoint(touchPosition);
 			//touchPosition = AspectUtility.mousePosition;
-			touchPosition = AspectUtility.mouseInvertPosition;
-			Debug.Log("clic gauche en ("+Input.mousePosition.x+" , "+Input.mousePosition.y+")");
+			//touchPosition = AspectUtility.mouseInvertPosition;
+			/*Debug.Log("clic gauche en ("+Input.mousePosition.x+" , "+Input.mousePosition.y+")");
 			Debug.Log("***clic gauche converti ("+touchPosition.x+" , "+touchPosition.y+")");
-			Debug.Log("perso centre en ("+gameObject.collider2D.bounds.center.x+" , "+gameObject.collider2D.bounds.center.y+")");
+			Debug.Log("perso centre en ("+gameObject.collider2D.bounds.center.x+" , "+gameObject.collider2D.bounds.center.y+")");*/
 
 			//affichage pour debug
 			InGameGUI.setMessage("perso centre en ("+gameObject.collider2D.bounds.center.x+" , "+gameObject.collider2D.bounds.center.y+")",
 			                     "***clic gauche converti ("+touchPosition.x+" , "+touchPosition.y+")");
 
-			if(gameObject.collider2D.bounds.Contains(new Vector2(Input.mousePosition.x,Input.mousePosition.y))){
+			if(gameObject.collider2D.bounds.Contains(new Vector2(touchPosition.x,touchPosition.y))){
 
 				Debug.Log("*************************------>   clic gauche sur le perso !!");
 			}
