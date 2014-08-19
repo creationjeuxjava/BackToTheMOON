e02c31c7c51b2e1f4eebd4786f9a9436945de021
JohnCarmack <faouzy.chaib@gmail.com>
2014-08-19 03:53:52 +0200

    pti script de test en mouvement

---
+++ Assets/Scripts/TestBougeShader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestBougeShader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+            transform.position = new Vector3(0, Mathf.Lerp(0, 5, Time.time / 5), 0);
+
+	}
+}
