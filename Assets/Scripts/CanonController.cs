533792a7231df138a20ae1a3fead6f9c4e0eca7c
alto971 <gwadaldesign@gmail.com>
2014-11-02 21:11:59 +0100

    implémentation canon
    au clic sur le canon tout le monde bouge...pas le player

---
+++ Assets/Scripts/CanonController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanonController : MonoBehaviour {
+
+	//public GameObject gameController;
+	private bool isMoving = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		Debug.Log ("***************  souris clic  sur : "+gameObject.name+" on va tirer");
+		GameController.LaunchPlayer();
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,16 +10,22 @@ public class GameController : MonoBehaviour {
 	public GameObject meteorite;
 	private int nbreMeteor = 5;
 
+	private GameObject world;//conteneur du world
+	private static bool isWorldMoving = false;
+
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("on crée le monde !!");
-
+		world = GameObject.FindGameObjectWithTag ("World");
 		createWorld ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (isWorldMoving == true) {
+				
+			world.transform.Translate (new Vector3 (0,-0.2f,0));
+		}
 	}
 
 
@@ -29,9 +35,16 @@ public class GameController : MonoBehaviour {
 		for(int i = 0 ; i < nbreMeteor ; i++){
 			Vector3 spawnPosition = new Vector3 (Random.Range(-45,56),Random.Range(0,30),0);
 			Quaternion spawnRotation =  Quaternion.identity;
-			Instantiate(meteorite, spawnPosition, spawnRotation);
+			GameObject sprite = Instantiate(meteorite, spawnPosition, spawnRotation) as GameObject;
+			sprite.transform.parent = world.transform;
 			
 		}
 
+	}
+
+
+	//lancer le joueur et donc faire avancer le monde !!
+	public static void LaunchPlayer (){
+		isWorldMoving = true;
 	}
 }
