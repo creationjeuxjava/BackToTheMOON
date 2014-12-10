c84e6f4c31856d0d48cf49ca1590aac1361bc2fa
alto971 <gwadaldesign@gmail.com>
2014-12-10 23:03:38 +0100

    exit les debug.log
    - pour voir si FPS est ok

--- Assets/Scripts/DestroyByOutOffBox.cs
+++ Assets/Scripts/DestroyByOutOffBox.cs
@@ -11,13 +11,13 @@ public class DestroyByOutOffBox : MonoBehaviour {
 
 	//TODO améliorer ce point ...pour l'instant seul le canon est détecté !!
 	void OnCollisionEnter2D(Collision2D other) {
-		Debug.Log("Collision other est sorti : "+other.gameObject.name +" qui était en z "+other.transform.position.z);
+		//Debug.Log("Collision other est sorti : "+other.gameObject.name +" qui était en z "+other.transform.position.z);
 		Destroy(other.gameObject);
 
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		Debug.Log("TRIGGER ==>Collision other est sorti : "+other.gameObject.name +" qui était en z "+other.transform.position.z);
+		//Debug.Log("TRIGGER ==>Collision other est sorti : "+other.gameObject.name +" qui était en z "+other.transform.position.z);
 		Destroy(other.gameObject);
 		
 	}
