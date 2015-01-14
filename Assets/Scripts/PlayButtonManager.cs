4493c97682b4e8b03059ba664eba96d9c7d7b434
Thibault <Thibault@Thibault-PC.home>
2015-01-15 00:52:35 +0100

    - intégration du menu sans l'implémentation (pb de build)

---
+++ Assets/Scripts/PlayButtonManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButtonManager : MonoBehaviour {
+
+	// Use this for initialization
+	void StartGame () {
+		Application.LoadLevel ("firstLevel");
+	}
+}
