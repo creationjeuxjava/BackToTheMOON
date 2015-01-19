37d8995bce088e2332e598717246367a6326915c
Thibault <Thibault@192.168.1.7>
2015-01-19 18:19:47 +0100

    - implémentation du zoom sur le menu jouer

--- Assets/Scripts/PlayButtonManager.cs
+++ Assets/Scripts/PlayButtonManager.cs
@@ -3,8 +3,15 @@ using System.Collections;
 
 public class PlayButtonManager : MonoBehaviour {
 
+	public Animator zoomAnimator;
 	// Use this for initialization
 	public void StartGame () {
+		zoomAnimator = GameObject.Find ("Canvas/HandBackground").GetComponent<Animator>();
+		zoomAnimator.SetBool ("zoom", false);
+		Invoke ("loadFirstLevel",1);
+	}
+
+	public void loadFirstLevel() {
 		Application.LoadLevel ("firstLevel");
 	}
 }
