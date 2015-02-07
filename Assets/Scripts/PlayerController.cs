61f77657d40dce09b1c58a000b93460197d9028e
alto971 <gwadaldesign@gmail.com>
2015-02-07 23:09:50 +0100

    PlayerController : timeLeft Beans ajusté

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,6 +18,7 @@ public class PlayerController : MonoBehaviour {
 	private float speedPlayer = 0.3f;//0.7f;
 	public static bool isWithCask = false;
 	public static bool isWithShoe = false;
+	public static bool isWithBeans = false;
 	private bool isItemActivated = false;
 	public static bool isFlyBegin = false;
 
@@ -123,7 +124,7 @@ public class PlayerController : MonoBehaviour {
 
 			transform.Translate(translation);
 
-			if(isItemActivated) this.checkTimeItemLeft();
+			if(isItemActivated) this.checkTimeItemLeft();//vérifie si l'item est encore valide !
 		}
 		if (GameController.isGamePaused ())
 						audio.Pause ();
@@ -166,6 +167,7 @@ public class PlayerController : MonoBehaviour {
 			isWithCask = true;
 			isWithShoe = false;
 			isItemActivated = true;
+			timeLeft = 5f;
 			anim.SetBool("withCask",true);
 			
 			Sprite casqueSprite = Resources.Load("Sprites/persocasque", typeof(Sprite)) as Sprite;
@@ -182,6 +184,7 @@ public class PlayerController : MonoBehaviour {
 			other.gameObject.SetActive(false);
 			isWithShoe = true;
 			isWithCask = false;
+			timeLeft = 5f;
 			isItemActivated = true;
 			anim.SetBool("withShoes",true);
 			
@@ -205,6 +208,8 @@ public class PlayerController : MonoBehaviour {
 			//Debug.Log ("***************  collision avec beans ");
 			isWithShoe = false;
 			isWithCask = false;
+			isWithBeans = true;
+			timeLeft = 1f;
 			isItemActivated = true;
 			anim.SetTrigger("goFlageollet");
 			other.gameObject.SetActive(false);
@@ -271,6 +276,7 @@ public class PlayerController : MonoBehaviour {
 
 		isWithShoe = false;
 		isWithCask = false;
+		isWithBeans = false;
 		anim.SetBool("withCask",false);
 		anim.SetBool("withShoes",false);
 		currentState = State.naked;
