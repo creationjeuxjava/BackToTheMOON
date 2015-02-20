8a83cc2aa882362c8f01da0f5ae6d476677d3f01
alto971 <gwadaldesign@gmail.com>
2015-02-20 13:51:21 +0100

    Player: mise en place de l'input accelerometer

---
+++ Assets/Scripts/Player/AccelerometerInput.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccelerometerInput : MonoBehaviour {
+
+	public bool isInputActive = false;
+	public float speed = 50f;
+
+	Vector3 dir;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (isInputActive) {
+			if (PlayerController.isFlying && !GameController.isGamePaused ()) {
+				dir.x = Input.acceleration.x;
+				dir *= Time.deltaTime;
+				transform.Translate (dir * speed);
+			}	
+		
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/Player/TouchInput.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchInput : MonoBehaviour {
+
+	public bool isInputActive = false;
+	public float speed = 50f;
+	
+	Vector3 dir;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
