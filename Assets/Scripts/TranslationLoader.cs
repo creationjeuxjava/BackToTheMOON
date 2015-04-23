456c7ba2eb18912b1b5d0b200e548633e5f34b46
alto971 <gwadaldesign@gmail.com>
2015-04-24 01:20:25 +0200

    Language : extension du principe

--- Assets/Scripts/TranslationLoader.cs
+++ Assets/Scripts/TranslationLoader.cs
@@ -6,9 +6,16 @@ using System.Collections.Generic;
 
 public class TranslationLoader : MonoBehaviour {
 
+
+	private const string ROOT_PATH = "XML/GUI_";
+	private XmlNode languageInfos;
+
 	// Use this for initialization
 	void Start () {
-		loadLanguage("fr");
+		loadLanguage("fr");//fr-en-es  :les 3 codes de langue 
+
+		loadMenuTranslation ();//juste pour la scene Menu + Levels
+		loadButtonsTranslation ();// pour tous les boutons de la GUI, et autre élmts UI
 	
 	}
 	
@@ -22,24 +29,50 @@ public class TranslationLoader : MonoBehaviour {
 	 */
 	public void loadLanguage(string codeLanguage){
 				Debug.Log (this + " on charge la langue " + codeLanguage);
-				TextAsset textAsset = (TextAsset)Resources.Load ("XML/GUI_" + codeLanguage, typeof(TextAsset));
+				TextAsset textAsset = (TextAsset)Resources.Load (ROOT_PATH + codeLanguage, typeof(TextAsset));
 				XmlDocument xmldoc = new XmlDocument ();
 				xmldoc.LoadXml (textAsset.text);//on charge le fichier
 		
 				XmlNodeList languagesList = xmldoc.GetElementsByTagName ("language"); // tableau des noeuds "level"
 
-				XmlNode languageInfos = languagesList [0];
+				languageInfos = languagesList [0];
 				//Debug.Log (this + " on recup  " + languageInfos.InnerXml);
 
-				XmlNodeList languageInfoscontent = languageInfos.ChildNodes;//récup de ses enfants !!
-				foreach (XmlNode objectInfo in languageInfoscontent) {
-		
-					//float gravity = float.Parse(objectInfo.Attributes["gravity"].Value);
-					string nameWorld = objectInfo.Attributes["titre"].Value;
-					string descriptif = objectInfo.InnerText;
-					Debug.Log (this + " on recup  "+ nameWorld +" : " + descriptif);
-		
-		
-				}
+				
+	}
+
+	public void loadMenuTranslation(){
+
+		XmlNodeList languageInfoscontent = languageInfos.ChildNodes;//récup de ses enfants !!
+		foreach (XmlNode objectInfo in languageInfoscontent) {
+			
+			if(objectInfo.Attributes["type"].Value.Equals("GUILevelBlock")){
+				string nameWorld = objectInfo.Attributes["titre"].Value;
+				string descriptif = objectInfo.InnerText;
+				Debug.Log (this + " on recup  "+ nameWorld +" : " + descriptif);
+				
+			}
+			
+			
+			
+		}
+	}
+
+	public void loadButtonsTranslation(){
+
+		XmlNodeList languageInfoscontent = languageInfos.ChildNodes;//récup de ses enfants !!
+		foreach (XmlNode objectInfo in languageInfoscontent) {
+			
+			if(objectInfo.Attributes["type"].Value.Equals("GUIOneShot")){
+				string type = objectInfo.Attributes["type"].Value;
+				string descriptif = objectInfo.InnerText;
+				Debug.Log (this + " on recup  "+ type +" : " + descriptif);
+				
+			}
+			
+			
+			
+		}
+
 	}
 }
